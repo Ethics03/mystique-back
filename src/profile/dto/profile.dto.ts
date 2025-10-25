be30a13162df6bf548c099fa5c63673c503f7510
Ethics03 <honey032004@gmail.com>
2025-10-26 02:11:11 +0530

    feat: added profile dtos

---
+++ src/profile/dto/profile.dto.ts
@@ -0,0 +1,57 @@
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  Matches,
+  IsOptional,
+} from '@nestjs/class-validator';
+
+export class CreateProfileDto {
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[0-9]{10}$/, { message: 'Contact must be 10 digits' })
+  contact: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[0-9]{12}$/, { message: 'Aadhaar must be 12 digits' })
+  aadhaar: string;
+
+  @IsString()
+  @IsNotEmpty()
+  aadhaarFileUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  collegeIdUrl: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 200)
+  collegeName: string;
+}
+
+export class UpdateProfileDto {
+  @IsOptional()
+  @IsString()
+  @Matches(/^[0-9]{10}$/, { message: 'Contact must be 10 digits' })
+  contact?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^[0-9]{12}$/, { message: 'Aadhaar must be 12 digits' })
+  aadhaar?: string;
+
+  @IsOptional()
+  @IsString()
+  aadhaarFileUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  collegeIdUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  @Length(3, 200)
+  collegeName?: string;
+}
